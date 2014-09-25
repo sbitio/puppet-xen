8a21724d9697a9efb9e1c67e2edc1e27560dd4ac
Jonathan Araña Cruz <jonhattan@faita.net>
2014-09-25 20:32:07 +0200

    Add domu class: manage kernel package and pygrub.

---
+++ manifests/domu.pp
@@ -0,0 +1,26 @@
+# From http://wiki.debian.org/PyGrub
+class xen::domu (
+  $ensure         = $xen::ensure,
+  $kernel_package = $xen::params::domu_kernel_package,
+  $purge_packages = $xen::params::domu_purge_packages,
+) inherits xen::params {
+
+  package {$kernel_package:
+    ensure => present,
+  }
+  package { $purge_packages:
+    ensure => purged,
+  }
+
+  # PyGrub.
+  file {'/boot/grub':
+    ensure  => directory,
+    mode    => 0444,
+  }
+  file {'/boot/grub/menu.lst':
+    ensure => present,
+    content => template('xen/domU/grub/menu.lst'),
+  }
+
+}
+
